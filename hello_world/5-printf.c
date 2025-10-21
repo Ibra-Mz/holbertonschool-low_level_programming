744e913af92568ad33ff173fdc991d33e91ab12a
ibrahim <ibrahimmazari165@gmail.com>
2025-10-21 08:11:48 +0000

    Make 5 work

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,10 +3,10 @@
 * main - print a message using prinft
 * Description : the fun print the phrase given
 * using printf fun followed by new line
-* Return 0 : success 
+* Return 0 : success
 */
 int main(void)
 {
 printf("\"with proper grammer, but the outcome is a piece of art")
-return (0);  
+return (0);
 }
