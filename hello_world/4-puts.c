1bb305eb5853a7bd0e45ae7c51e430cad9307577
ibrahim <ibrahimmazari165@gmail.com>
2025-10-21 08:02:54 +0000

    Make 4 work

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - prints a message using puts
  *
- * Description: This function prints "Programming is like building a multilingual puzzle"
+ * Description: This function prints "Programming is like building"
  * using the puts function, followed by a new line.
  *
  * Return: Always 0 (Success)
