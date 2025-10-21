69a821a0309f48f5e415ad8686b4cfc918385eda
ibrahim <ibrahimmazari165@gmail.com>
2025-10-21 07:48:14 +0000

    Make 4 work

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/*
+*print a text given
+*followed by new line
+*used fun are puts 
+*ends with 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
