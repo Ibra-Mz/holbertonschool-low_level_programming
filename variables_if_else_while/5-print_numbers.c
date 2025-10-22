8ba5af07c3e4f384a39f057ba6e24248360d7de4
ibrahim <ibrahimmazari165@gmail.com>
2025-10-22 05:40:40 +0000

    test 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - starting point of the program
+*
+* Declare the variable i
+* loop to display number 0-9
+*
+* Return : o means succes
+*/
+int main(void)
+{
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
