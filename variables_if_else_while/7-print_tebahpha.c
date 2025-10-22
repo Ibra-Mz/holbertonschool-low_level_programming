700cca28014e63e48dca62421611a59d4f22452f
ibrahim <ibrahimmazari165@gmail.com>
2025-10-22 07:22:17 +0000

    test 7

---
+++ variables_if_else_while/7-print_tebahpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - starting point of the program
+ *
+ * Description: Prints the alphabet in reverse
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+for (i = 'z'; i < a, i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
