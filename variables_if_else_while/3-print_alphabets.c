ecfa3183a151724ab52c3f2db780c7bf49fad04e
ibrahim <ibrahimmazari165@gmail.com>
2025-10-22 05:26:15 +0000

    test 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - starting point of the program
+ *
+ * Description: Prints the alphabet in lowercase,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i); 
+}
+for(i = 'A'; i <= 'Z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
