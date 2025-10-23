2fdb00f67439b3c7de1a8685abb26ced3c856f5f
ibrahim <ibrahimmazari165@gmail.com>
2025-10-23 08:42:10 +0000

    test

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* print alphabet - followed by new line 
+* print alphabet - followed by new line
 */
 void print_alphabet(void)
 {
 char c;
 
 for (c = 'a'; c <= 'z'; c++)
-_putchar(c);   
+_putchar(c);
 
-_putchar('\n');    
+_putchar('\n');
 }
