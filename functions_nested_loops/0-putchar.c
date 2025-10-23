f7f5cb171eb5f3c2f353c6f2e542f6957c0dd9cd
ibrahim <ibrahimmazari165@gmail.com>
2025-10-23 07:57:05 +0000

    test 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-    char text[] = "_putchar\n";
-    int i;
+char text[] = "_putchar\n";
+int i;
 
-    for (i = 0; text[i] != '\0'; i++)
-    {
-        _putchar(text[i]);
-    }
-    return (0);
+for (i = 0; text[i] != '\0'; i++)
+{
+_putchar(text[i]);
+}
+return (0);
 }
