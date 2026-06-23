ae9502ff684f2bb5862d5763e293b3a58e453f60
ibrahim <ibrahimmazari165@gmail.com>
2026-06-23 11:57:06 +0200

    Make 5 work

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints a message using printf
+ *
+ * Description: This function prints the phrase
+ * "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
