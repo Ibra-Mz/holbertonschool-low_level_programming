89ab3b09db14e75ab891f38a98eaf179edf44b57
ibrahim <ibrahimmazari165@gmail.com>
2026-06-23 20:17:49 +0200

    Make 5 work

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+i = 0;
+while (i <= 9)
+{
+putchar (i + '0');
+i++;
+}
+putchar ('\n');
+return (0);
+}
