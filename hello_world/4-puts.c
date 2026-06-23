85a6acc8baf267321a3145e0b46d529d9fa593a8
ibrahim <ibrahimmazari165@gmail.com>
2026-06-23 11:49:38 +0200

    Make 4 works

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints a message using puts
+ *
+ * Description: This function prints "Programming is like building"
+ * using the puts function, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
