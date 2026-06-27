29a9174fa59eb02350f8be4e9e6767d88b6ccc3a
ibrahim <ibrahimmazari165@gmail.com>
2026-06-27 23:20:35 +0200

    make 10 work betty style

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,24 +8,21 @@ void print_triangle(int size)
 {
 int i;
 int j;
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
-
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            _putchar(' ');
-        }
-
-        for (j = 0; j <= i; j++)
-        {
-            _putchar('#');
-        }
-
-        _putchar('\n');
-    }
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+_putchar(' ');
+}
+for (j = 0; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
 }
