db5fc704a0c93ac8d7253d4d62d67fa34b712307
ibrahim <ibrahimmazari165@gmail.com>
2026-06-27 23:03:18 +0200

    Make 9 work

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints FizzBuzz from 1 to 100
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (((i % 3) == 0) && ((i % 5) == 0))
+{
+printf("%s ", "FizzBuzz ");
+}
+else if ((i % 5) == 0)
+{
+printf("%s ", "Buzz ");
+}
+else if ((i % 3) == 0)
+{
+printf("%s ", "Fizz ");
+}
+else
+printf("%d ", i);
+}
+printf("\n");
+return (0);
+}
