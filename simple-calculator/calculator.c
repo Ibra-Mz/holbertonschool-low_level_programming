9c280f87812c23213228c54cc6de0e09ff31a798
ibrahim <ibrahimmazari165@gmail.com>
2026-06-30 16:59:01 +0200

    first step intro the calculator process

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - simple calculator
+ * Return: 0
+ */
+
+int main(void)
+{
+int choice = 1;
+/**
+int A = 0;
+int B = 0;
+int result = 0;
+*/
+printf("simple calculator\n");
+while (choice != 0)
+{
+printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n");
+printf("choice?\n");
+scanf("%d", &choice);
+if (choice == 0)
+{
+printf("Bye!\n");
+}
+}
+return (0);
+}
