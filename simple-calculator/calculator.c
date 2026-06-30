ebe13e3277baba4626c44c981ca1458c8039069d
ibrahim <ibrahimmazari165@gmail.com>
2026-06-30 17:11:03 +0200

    task 1 for simple calculator

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -16,11 +16,15 @@ printf("simple calculator\n");
 while (choice != 0)
 {
 printf("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n");
-printf("choice?\n");
+printf("choice: \n");
 scanf("%d", &choice);
 if (choice == 0)
 {
 printf("Bye!\n");
+}
+if (choice < 0 || choice > 4)
+{
+printf("Invalid choice\n");
 }
 }
 return (0);
