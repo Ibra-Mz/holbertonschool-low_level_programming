cebc924a0d1458474bb4584ff0c397e54280f33d
ibrahim <ibrahimmazari165@gmail.com>
2026-07-02 13:27:49 +0200

    Make 5 work

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - function that changes lowercase of a string to upper
+ * @s: string to be changed
+ *
+ * Return: a string
+*/
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
