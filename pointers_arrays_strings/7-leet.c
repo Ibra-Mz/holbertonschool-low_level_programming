3eef156f311065a00222b44e6fcac2554addc299
ibrahim <ibrahimmazari165@gmail.com>
2026-07-02 20:08:21 +0200

    fixing the 7 fuction

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,8 +8,8 @@
 char *leet(char *s)
 {
 	int i, j;
-	char letters = "aAeEoOtTlL";
-	char numbers = "4433007711";
+	char *letters = "aAeEoOtTlL";
+	char *numbers = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
