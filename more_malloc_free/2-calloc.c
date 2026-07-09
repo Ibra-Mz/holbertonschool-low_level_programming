069961170d142042bde1a5ad9ec448f9c5cb6399
ibrahim <ibrahimmazari165@gmail.com>
2026-07-09 13:00:48 +0200

    make 2 work

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -15,6 +15,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 	unsigned int total_size;
+	unsigned char *bytes;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -24,8 +26,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	/* Initialize allocated memory to zero */
-	memset(ptr, 0, total_size);
+	/* Initialize allocated memory to zero, manually */
+	bytes = ptr;
+	for (i = 0; i < total_size; i++)
+		bytes[i] = 0;
 
 	return (ptr);
 }
