0232c305f5747df0e79eb156ed2b0d23dc9e35be
ibrahim <ibrahimmazari165@gmail.com>
2026-07-12 22:10:56 +0200

    make 3 work

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,6 +6,7 @@
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
+ * description: struct dog
  */
 
 struct dog
@@ -17,5 +18,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 
 #endif
